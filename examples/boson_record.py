40d2c592d6d5c2225755d6ac2873006072a876ba
Sumega Mandadi <sumi1194@gmail.com>
2025-11-11 12:56:21 -1000

    flake8/pylint boson_record.py

--- examples/boson_record.py
+++ examples/boson_record.py
@@ -1,35 +1,37 @@
 '''Example script to record thermal imagery using FLIR Boson
 Radiometric camera.
 
-This script demonstrates how to use the 'BosonCapture' class from 
+This script demonstrates how to use the 'BosonCapture' class from
 the 'heatseek' package to record both radiometric data and a normalized
 viewable video file from a connected FLIR Boson camera.
 
-Usage: 
+Usage:
     python boson_record.py \
     --raw_output_path path/to/output.npy \
     --video_output_path path/to/output.mp4 \
     --bosonsdk_path path/to/BosonSDK/SDK_USER_PERMISSIONS \
     --recording_time 60
 
-If no arguments are provided, default filenames are used and script records 
+If no arguments are provided, default filenames are used and script records
 for 10s.
-'''   
+'''
 
-from heatseek.boson_capture import BosonCapture
 import argparse
 from time import sleep
+from heatseek.boson_capture import BosonCapture
 
 
 def main():
     '''Run exmaple thermal recording using BosonCapture
 
-    Parses arguments, initializes camera, starts recording for specified 
+    Parses arguments, initializes camera, starts recording for specified
     duration, then stops and releases camera.
 
     Command-line args:
-        --raw_output_path (str, opt): Filepath to save radiometric output (.npy)
-        --video_output_path (str, opt): Filepath to save normalized video (.mp4)
+        --raw_output_path (str, opt): Filepath to save
+            radiometric output (.npy)
+        --video_output_path (str, opt): Filepath to save
+            normalized video (.mp4)
         --bosonsdk_path (str, opt) Filepath to boson sdk folder
         --recording_time (int, opt): Duration of recording in seconds
 
@@ -51,7 +53,7 @@ def main():
                         type=int,
                         help='time in s to record')
     args = parser.parse_args()
-   
+
     camera = BosonCapture()
     camera.start_recording(raw=args.raw_output_path,
                            norm=args.video_output_path)
@@ -59,6 +61,6 @@ def main():
     camera.stop_recording()
     camera.release_camera()
 
-  
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
